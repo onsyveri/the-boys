2ef8e978f0782d6fafc8d63839c2680f07f5c40a
Ole Syverinsen <ole.nikolai.ravn.syverinsen@no.experis.com>
2022-11-03 12:01:40 +0100

    changed private to false in package.json

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import Navbar from './components/navbar/Navbar';
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import {BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from './views/HomePage';
 import MembersPage from './views/MembersPage';
 import Profile from './views/Profile';
