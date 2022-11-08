1c8fbf40f6c610804ea86849c4db4da492db3e3f
Ole Syverinsen <ole.nikolai.ravn.syverinsen@no.experis.com>
2022-11-08 13:27:38 +0100

    New homepage design. Also added a hover effect on members for desktop

--- src/views/HomePage.jsx
+++ src/views/HomePage.jsx
@@ -1,15 +1,18 @@
-import banner from "../images/yuno-peek.png"
+import pepPunch from '../images/pngegg.png'
 
 const HomePage = () => {
     return (
-        <>
-            <h1 id="homeH1">TILT</h1>
+        <div id="homeContent">
+            <h1 id="homeH1">IMAGINE A PLACE...</h1>
+            <p id="homeParag">
+                ...where you can belong to a school club, a gaming group, or a worldwide art community. 
+                Where just you and a handful of friends can spend time together. 
+                A place that makes it easy to talk every day and hang out more often.
+            </p>
             <div id="home">
-                <img id="homeImg3" src="https://media4.giphy.com/media/YbHnru6KfNiUGeNeCf/giphy.gif?cid=ecf05e47d7d7k0y1ia2cogz7vrm2jd4w3m1u6el2g4vw4j9l&rid=giphy.gif&ct=g" alt="home gif" />
-                <img id="homeImg" src={banner} alt="home gif" />
-                <img id="homeImg2" src="https://media4.giphy.com/media/dxld1UBIiGuoh31Fus/giphy.gif?cid=ecf05e47r5hkit7nt1sydby8orn6idg07092ky07kb3mtt4d&rid=giphy.gif&ct=g" alt="home gif" />
+                <img id="homeImg" src={pepPunch} alt="home banner" />
             </div>
-        </>
+        </div>
     )
 }
 
