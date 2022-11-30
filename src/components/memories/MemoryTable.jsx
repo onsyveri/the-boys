dacdb280f02ea83d06b1b12d11605ef6a884360f
Ole Syverinsen <ole.nikolai.ravn.syverinsen@no.experis.com>
2022-11-30 14:45:54 +0100

    On memories page, added member name on hover and link to profile detail page

--- src/components/memories/MemoryTable.jsx
+++ src/components/memories/MemoryTable.jsx
@@ -4,6 +4,7 @@ import zeus from '../../images/schultz-zeus.png';
 import viktor from '../../images/viktor-despair.png';
 import peaky from '../../images/peaky-blinders.png';
 import plan from '../../images/the-plan.png';
+import { NavLink } from 'react-router-dom';
 
 const MemoryTable = () => {
 
@@ -25,10 +26,10 @@ const MemoryTable = () => {
                 <div id="memMembers" className="flex mb-5 -space-x-4">
                     Members present:
                     <div id="memProfiles"  className="flex mb-5 -space-x-4">
-                        <img className="w-10 h-10 rounded-full border-2 border-indigo-300 dark:border-gray-800" src="https://media1.giphy.com/media/U3UP4fTE6QfuoooLaC/giphy.gif?cid=790b761120405ec0a71212bc059b081f34735ab76411c7e6&rid=giphy.gif&ct=g" alt="gif" />
-                        <img className="w-10 h-10 rounded-full border-2 border-indigo-300 dark:border-gray-800" src="https://media.tenor.com/Dzj2OFaZVL4AAAAC/friday.gif" alt="gif" />
-                        <img className="w-10 h-10 rounded-full border-2 border-indigo-300 dark:border-gray-800" src="https://media.tenor.com/Wg1jbWqmB8IAAAAC/nicholas-brown.gif" alt="gif" />
-                        <img className="w-10 h-10 rounded-full border-2 border-indigo-300 dark:border-gray-800" src="https://media.tenor.com/nDRHIQbzGz8AAAAC/dr-stone-tsukasa.gif" alt="gif" />
+                        <NavLink to={"/profile?id=1"}><img title="Shwarma Queen" className="w-10 h-10 rounded-full border-2 border-indigo-300 dark:border-gray-800 hover:border-white" src="https://media1.giphy.com/media/U3UP4fTE6QfuoooLaC/giphy.gif?cid=790b761120405ec0a71212bc059b081f34735ab76411c7e6&rid=giphy.gif&ct=g" alt="gif" /> </NavLink>
+                        <NavLink to={"/profile?id=5"}> <img title="Ruen" className="w-10 h-10 rounded-full border-2 border-indigo-300 dark:border-gray-800 hover:border-white" src="https://media.tenor.com/Dzj2OFaZVL4AAAAC/friday.gif" alt="gif" /> </NavLink>
+                        <NavLink to={"/profile?id=7"}> <img title="Titow" className="w-10 h-10 rounded-full border-2 border-indigo-300 dark:border-gray-800 hover:border-white" src="https://media.tenor.com/Wg1jbWqmB8IAAAAC/nicholas-brown.gif" alt="gif" /> </NavLink>
+                        <NavLink to={"/profile?id=10"}> <img title="Typhex" className="w-10 h-10 rounded-full border-2 border-indigo-300 dark:border-gray-800 hover:border-white" src="https://media.tenor.com/nDRHIQbzGz8AAAAC/dr-stone-tsukasa.gif" alt="gif" /> </NavLink>
                     </div> 
                 </div>
             </div>
@@ -47,11 +48,11 @@ const MemoryTable = () => {
                 <div id="memMembers" className="flex mb-5 -space-x-4">
                     Members present:
                     <div id="memProfiles"  className="flex mb-5 -space-x-4">
-                        <img className="w-10 h-10 rounded-full border-2 border-indigo-300 dark:border-gray-800" src="https://media.tenor.com/ETtJihFiaOAAAAAd/one-piece-usopp.gif" alt="gif" />
-                        <img className="w-10 h-10 rounded-full border-2 border-indigo-300 dark:border-gray-800" src="https://media.tenor.com/Wg1jbWqmB8IAAAAC/nicholas-brown.gif" alt="gif" />
-                        <img className="w-10 h-10 rounded-full border-2 border-indigo-300 dark:border-gray-800" src="https://media.tenor.com/nDRHIQbzGz8AAAAC/dr-stone-tsukasa.gif" alt="gif" />
-                        <img className="w-10 h-10 rounded-full border-2 border-indigo-300 dark:border-gray-800" src="https://media.tenor.com/4dt6GlT1KBMAAAAC/jojo-jjba.gif" alt="gif" />
-                        <a className="flex justify-center items-center w-10 h-10 text-xs font-medium text-white bg-gray-700 rounded-full border-2 border-indigo-300 hover:bg-gray-600 dark:border-gray-800" href="/the-boys/#/members">+13</a>
+                        <NavLink to={"/profile?id=0"}> <img title="BenjBjar" className="w-10 h-10 rounded-full border-2 border-indigo-300 dark:border-gray-800 hover:border-white" src="https://media.tenor.com/ETtJihFiaOAAAAAd/one-piece-usopp.gif" alt="gif" /> </NavLink>
+                        <NavLink to={"/profile?id=7"}> <img title="Titow" className="w-10 h-10 rounded-full border-2 border-indigo-300 dark:border-gray-800 hover:border-white" src="https://media.tenor.com/Wg1jbWqmB8IAAAAC/nicholas-brown.gif" alt="gif" /> </NavLink>
+                        <NavLink to={"/profile?id=10"}> <img title="Typhex" className="w-10 h-10 rounded-full border-2 border-indigo-300 dark:border-gray-800 hover:border-white" src="https://media.tenor.com/nDRHIQbzGz8AAAAC/dr-stone-tsukasa.gif" alt="gif" /> </NavLink>
+                        <NavLink to={"/profile?id=4"}> <img title="Schultzern" className="w-10 h-10 rounded-full border-2 border-indigo-300 dark:border-gray-800 hover:border-white" src="https://media.tenor.com/4dt6GlT1KBMAAAAC/jojo-jjba.gif" alt="gif" /> </NavLink>
+                        <a className="flex justify-center items-center w-10 h-10 text-xs font-medium text-white bg-gray-700 rounded-full border-2 border-indigo-300 hover:bg-gray-600 dark:border-gray-800 hover:border-white" href="/the-boys/#/members">+13</a>
                     </div> 
                 </div>
             </div>
@@ -72,11 +73,11 @@ const MemoryTable = () => {
                     <div id="memMembers" className="flex mb-5 -space-x-4">
                         Members present:
                         <div id="memProfiles"  className="flex mb-5 -space-x-4">
-                            <img className="w-10 h-10 rounded-full border-2 border-indigo-300 dark:border-gray-800" src="https://media1.giphy.com/media/U3UP4fTE6QfuoooLaC/giphy.gif?cid=790b761120405ec0a71212bc059b081f34735ab76411c7e6&rid=giphy.gif&ct=g" alt="gif" />
-                            <img className="w-10 h-10 rounded-full border-2 border-indigo-300 dark:border-gray-800" src="https://media.tenor.com/baphOx2zAgUAAAAC/mob-psycho.gif" alt="gif" />
-                            <img className="w-10 h-10 rounded-full border-2 border-indigo-300 dark:border-gray-800" src="https://media.tenor.com/Wg1jbWqmB8IAAAAC/nicholas-brown.gif" alt="gif" />
-                            <img className="w-10 h-10 rounded-full border-2 border-indigo-300 dark:border-gray-800" src="https://media.tenor.com/tPkh9euN9e4AAAAC/itachi-uchiha-naruto.gif" alt="gif" />
-                            <img className="w-10 h-10 rounded-full border-2 border-indigo-300 dark:border-gray-800" src="https://media.tenor.com/4dt6GlT1KBMAAAAC/jojo-jjba.gif" alt="gif" />
+                            <NavLink to={"/profile?id=1"}> <img title="Shwarma Queen" className="w-10 h-10 rounded-full border-2 border-indigo-300 dark:border-gray-800 hover:border-white" src="https://media1.giphy.com/media/U3UP4fTE6QfuoooLaC/giphy.gif?cid=790b761120405ec0a71212bc059b081f34735ab76411c7e6&rid=giphy.gif&ct=g" alt="gif" /> </NavLink>
+                            <NavLink to={"/profile?id=6"}> <img title="Vladmit" className="w-10 h-10 rounded-full border-2 border-indigo-300 dark:border-gray-800 hover:border-white" src="https://media.tenor.com/baphOx2zAgUAAAAC/mob-psycho.gif" alt="gif" /> </NavLink>
+                            <NavLink to={"/profile?id=7"}> <img title="Titow" className="w-10 h-10 rounded-full border-2 border-indigo-300 dark:border-gray-800 hover:border-white" src="https://media.tenor.com/Wg1jbWqmB8IAAAAC/nicholas-brown.gif" alt="gif" /> </NavLink>
+                            <NavLink to={"/profile?id=3"}> <img title="Pishva" className="w-10 h-10 rounded-full border-2 border-indigo-300 dark:border-gray-800 hover:border-white" src="https://media.tenor.com/tPkh9euN9e4AAAAC/itachi-uchiha-naruto.gif" alt="gif" /> </NavLink>
+                            <NavLink to={"/profile?id=4"}> <img title="Schultzern" className="w-10 h-10 rounded-full border-2 border-indigo-300 dark:border-gray-800 hover:border-white" src="https://media.tenor.com/4dt6GlT1KBMAAAAC/jojo-jjba.gif" alt="gif" /> </NavLink>
                         </div> 
                     </div>
                 </div>
@@ -94,11 +95,11 @@ const MemoryTable = () => {
                 <div id="memMembers" className="flex mb-5 -space-x-4">
                     Members present:
                     <div id="memProfiles"  className="flex mb-5 -space-x-4">
-                        <img className="w-10 h-10 rounded-full border-2 border-indigo-300 dark:border-gray-800" src="https://media1.giphy.com/media/U3UP4fTE6QfuoooLaC/giphy.gif?cid=790b761120405ec0a71212bc059b081f34735ab76411c7e6&rid=giphy.gif&ct=g" alt="gif" />
-                        <img className="w-10 h-10 rounded-full border-2 border-indigo-300 dark:border-gray-800" src="https://media2.giphy.com/media/ERc5g8ThElMQM/giphy.gif?cid=ecf05e47ylsvgujyhonswqyxljezl0fm8w12tcl7waexymr1&rid=giphy.gif&ct=g" alt="gif" />
-                        <img className="w-10 h-10 rounded-full border-2 border-indigo-300 dark:border-gray-800" src="https://media2.giphy.com/media/H55l0bcEOOqWqlrNbt/giphy.gif?cid=ecf05e47tf3uslrjezjj4a5n94l8wh2j2lm4ngvrxdfsw697&rid=giphy.gif&ct=g" alt="gif" />
-                        <img className="w-10 h-10 rounded-full border-2 border-indigo-300 dark:border-gray-800" src="https://media.tenor.com/4dt6GlT1KBMAAAAC/jojo-jjba.gif" alt="gif" />
-                        <p className="flex justify-center items-center w-10 h-10 text-xs font-medium text-white bg-gray-700 rounded-full border-2 border-indigo-300 dark:border-gray-800">+1</p>
+                        <NavLink to={"/profile?id=1"}> <img title="Shwarma Queen" className="w-10 h-10 rounded-full border-2 border-indigo-300 dark:border-gray-800 hover:border-white" src="https://media1.giphy.com/media/U3UP4fTE6QfuoooLaC/giphy.gif?cid=790b761120405ec0a71212bc059b081f34735ab76411c7e6&rid=giphy.gif&ct=g" alt="gif" /> </NavLink>
+                        <NavLink to={"/profile?id=2"}> <img title="Snoke" className="w-10 h-10 rounded-full border-2 border-indigo-300 dark:border-gray-800 hover:border-white" src="https://media2.giphy.com/media/ERc5g8ThElMQM/giphy.gif?cid=ecf05e47ylsvgujyhonswqyxljezl0fm8w12tcl7waexymr1&rid=giphy.gif&ct=g" alt="gif" /> </NavLink>
+                        <NavLink to={"/profile?id=16"}> <img title="Dr. Jake" className="w-10 h-10 rounded-full border-2 border-indigo-300 dark:border-gray-800 hover:border-white" src="https://media2.giphy.com/media/H55l0bcEOOqWqlrNbt/giphy.gif?cid=ecf05e47tf3uslrjezjj4a5n94l8wh2j2lm4ngvrxdfsw697&rid=giphy.gif&ct=g" alt="gif" /> </NavLink>
+                        <NavLink to={"/profile?id=4"}> <img title="Schultzern" className="w-10 h-10 rounded-full border-2 border-indigo-300 dark:border-gray-800 hover:border-white" src="https://media.tenor.com/4dt6GlT1KBMAAAAC/jojo-jjba.gif" alt="gif" /> </NavLink>
+                        <p className="flex justify-center items-center w-10 h-10 text-xs font-medium text-white bg-gray-700 rounded-full border-2 border-indigo-300 dark:border-gray-800 hover:border-white">+1</p>
                     </div> 
                 </div>
             </div>
@@ -116,7 +117,7 @@ const MemoryTable = () => {
                 <div id="memMembers" className="flex mb-5 -space-x-4">
                     Members present:
                     <div id="memProfiles"  className="flex mb-5 -space-x-4">
-                        <img className="w-10 h-10 rounded-full border-2 border-indigo-300 dark:border-gray-800" src="https://media1.giphy.com/media/U3UP4fTE6QfuoooLaC/giphy.gif?cid=790b761120405ec0a71212bc059b081f34735ab76411c7e6&rid=giphy.gif&ct=g" alt="gif" />
+                        <NavLink to={"/profile?id=1"}><img title="Shwarma Queen" className="w-10 h-10 rounded-full border-2 border-indigo-300 dark:border-gray-800 hover:border-white" src="https://media1.giphy.com/media/U3UP4fTE6QfuoooLaC/giphy.gif?cid=790b761120405ec0a71212bc059b081f34735ab76411c7e6&rid=giphy.gif&ct=g" alt="gif" /></NavLink>
                     </div> 
                 </div>
             </div>
